531c45d10609c121107ee2460103c20ce23db499
DNS-HRA <44985923+DNS-HRA@users.noreply.github.com>
2022-09-17 17:57:56 +0200

    nicht benötigte Dateien entfernt

--- HTML+CSS/README.md
+++
@@ -1,5 +0,0 @@
-# Programmierung in HTMl+CSS
-Dieser Ordner enthält alle HTML und CSS Projekte
-
-## Ziel:
-- Verstehen von HTML und CSS
--- Javascript/Node.JS/webserver-package/delete.js
+++
@@ -1,7 +0,0 @@
-export function deleteAddress(addresses, id) {
-  const parsedId = parseInt(id, 10);
-  const filteredAddresses = addresses.filter(
-    (address) => address.id !== parsedId,
-  );
-  return filteredAddresses;
-}
